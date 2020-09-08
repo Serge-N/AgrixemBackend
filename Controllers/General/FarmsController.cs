2cad2528836461c1cbc40f7e3616f8f2ee873502
Serge Nalishiwa <51508252+Serge-N@users.noreply.github.com>
2020-09-08 17:13:40 +0200

    Remove security

--- Controllers/General/FarmsController.cs
+++ Controllers/General/FarmsController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using AgrixemAPI.Core.Models.General;
 using AgrixemAPI.Data;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace AgrixemAPI.Controllers.General
 {
